948fe20b98f49c2e3a85f2c0878d62e3d897e2a7
Olga Emelyanova <volha.emelianova@gmail.com>
2020-09-15 20:55:44 +0300

    Add custom concat and map

--- list.hs
+++ list.hs
@@ -2,9 +2,11 @@ lengthx :: [a] -> Int
 lengthx []     = 0
 lengthx (_:xs) = 1 + lengthx xs
 
+
 at' :: [a] -> Int -> a
 at' (x:xs) 0 = x
 at' (x:xs) i = at' xs (i-1) 
+
 
 reverseHelper :: [a] -> [a] -> [a]
 reverseHelper []     ys = ys
@@ -13,9 +15,21 @@ reverseHelper (x:xs) ys = reverseHelper xs (x:ys)
 reverse' :: [a] -> [a]
 reverse' xs = reverseHelper xs []
 
-concat' :: [a] -> [a] -> [a]
-concat' [] ys = ys
-concat' (x:xs) ys = x:(concat' xs ys)
--- the above is not tail recursion (I think); 
--- need to rewrite using tail recursion
 
+concat' :: [a] -> [a] -> [a]
+concat' xs []     = xs
+concat' xs (y:ys) = concat' (xs++[y]) ys
+
+
+concat'' :: [[a]] -> [a]
+concat'' []       = []
+concat'' [x]      = x
+concat'' (x:y:xs) = concat'' ((concat' x y):xs)
+
+
+fmapHelper :: (a -> b) -> [a] -> [b] -> [b]
+fmapHelper _ [] ready   = ready
+fmapHelper f (x:xs) ready = fmapHelper f xs (ready ++ [f x])
+
+fmap' :: (a -> b) -> [a] -> [b]
+fmap' f (x:xs) = fmapHelper f xs [f x]
